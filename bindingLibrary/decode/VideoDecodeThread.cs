f7ea716c1fa896356de297f197d250c679f2eb89
carlosjgr7 <carlosjgr7@gmail.com>
2021-06-11 16:11:23 -0400

    VideoDecodeThread

--- bindingLibrary/decode/VideoDecodeThread.cs
+++ bindingLibrary/decode/VideoDecodeThread.cs
@@ -1,6 +1,8 @@
 ﻿using Android.Media;
 using Android.Views;
 using Java.Lang;
+using System.Threading.Tasks;
+
 
 namespace bindingLibrary.decode
 {
@@ -14,14 +16,14 @@ namespace bindingLibrary.decode
         private FrameQueue videoFrameQueue;
         // esta funncion debo pasarla a un hilo para que corra en segundo plano
 
-        public void run()
+        public async Task run()
         {
             MediaCodec decoder = MediaCodec.CreateDecoderByType(mimeType);
             MediaFormat format = MediaFormat.CreateVideoFormat(mimeType, width, height);
 
             decoder.Configure(format, surface, null, 0);
             decoder.Start();
-            var bufferInfo =new MediaCodec.BufferInfo();
+            var bufferInfo = new MediaCodec.BufferInfo();
 
             while (/*Revisar si el hilo sigue andando*/true)
             {
@@ -49,35 +51,44 @@ namespace bindingLibrary.decode
                     catch (Exception e)
                     {
                         e.PrintStackTrace();
-                  }
+                    }
                 }
                 try
                 {
+
                     var outIndex = decoder.DequeueOutputBuffer(bufferInfo, 10000);
-                    if (outIndex == -2) {
-                        System.Console.WriteLine(TAG, "Decoder format changed: " + decoder.OutputFormat);
-                    } else
-                        {
-                        decoder.ReleaseOutputBuffer(outIndex, bufferInfo.size != 0);
-                        }
-               
-                    
-                }
-                catch (Exception e) {
+                    switch (outIndex)
+                    {
+                        case (int)MediaCodecInfoState.OutputFormatChanged:
+                            System.Console.WriteLine(TAG, "Decoder format changed: " + decoder.OutputFormat);
+                            break;
+                        case (int)MediaCodecInfoState.TryAgainLater:
+                            if (outIndex >= 0)
+                            {
+                                decoder.ReleaseOutputBuffer(outIndex, bufferInfo.Size != 0);
+                             }
+                            break;
+                    }
+                }   
+                catch (Exception e)
+                {
                     e.PrintStackTrace();
-              }
+                }
 
                 // All decoded frames have been rendered, we can stop playing now
-                if (bufferInfo.flags and MediaCodec.BUFFER_FLAG_END_OF_STREAM != 0) {
+                var flags = (int) bufferInfo.Flags;
+                var mediaCodeFlags = (int) MediaCodecBufferFlags.EndOfStream;
+
+                if ((flags != 0) && (mediaCodeFlags!=0)) {
                     System.Console.WriteLine(TAG, "OutputBuffer BUFFER_FLAG_END_OF_STREAM");
                     break;
                 }
 
-                }
+            }
             decoder.Stop();
             decoder.Release();
             videoFrameQueue.clear();
-            }
         }
     }
+}
 
